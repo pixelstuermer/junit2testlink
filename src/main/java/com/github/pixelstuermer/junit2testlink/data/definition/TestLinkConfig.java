71098deca9e85fa57d55f7e734f0ad7f6f258510
Philipp Montesano <philipp.montesano@icloud.com>
2020-01-03 13:33:40 +0100

    Added Spring-like documentation of .properties file

--- src/main/java/com/github/pixelstuermer/junit2testlink/data/definition/TestLinkConfig.java
+++ src/main/java/com/github/pixelstuermer/junit2testlink/data/definition/TestLinkConfig.java
@@ -12,12 +12,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum TestLinkConfig {
 
-    TEST_LINK_ENABLED("testlink.enabled"),
-    TEST_LINK_BASE_URI("testlink.base-uri"),
-    TEST_LINK_API_KEY("testlink.api-key"),
-    TEST_LINK_PLATFORM_ID("testlink.platform-id"),
-    TEST_LINK_PLAN_ID("testlink.plan-id"),
-    TEST_LINK_BUILD_ID("testlink.build-id");
+    TEST_LINK_ENABLED("report2testlink.enabled"),
+    TEST_LINK_BASE_URI("report2testlink.base-uri"),
+    TEST_LINK_API_KEY("report2testlink.api-key"),
+    TEST_LINK_PLATFORM_ID("report2testlink.platform-id"),
+    TEST_LINK_PLAN_ID("report2testlink.plan-id"),
+    TEST_LINK_BUILD_ID("report2testlink.build-id");
 
     private String propertiesFileKey;
 
